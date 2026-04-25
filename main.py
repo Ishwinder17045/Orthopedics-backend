57a8d38792721f0103ab7adec6bf04b878458ee2
Ishwinder17045 <ishwinder17045@gmail.com>
2026-04-25 18:11:19 +0530

    force cors fix

--- main.py
+++ main.py
@@ -16,18 +16,7 @@ app = FastAPI(title="Orthopaedics Healthcare API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5175",
-        "http://127.0.0.1:5175",
-        "https://orthofrontend.vercel.app"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"]
 )
 
 app.include_router(doctors.router)
