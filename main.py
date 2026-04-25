2e27a73a7d66c206facdc07f6f3de57e2c26f953
Ishwinder17045 <ishwinder17045@gmail.com>
2026-04-25 17:56:03 +0530

    fix cors for frontend

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:5175",
-        "http://127.0.0.1:5175"
+        "http://127.0.0.1:5175",
+        "https://orthofrontend.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
