e0526624e91e3ec9a500550ebdb53d09b2c93134
Ishwinder17045 <ishwinder17045@gmail.com>
2026-04-25 18:18:19 +0530

    final cors config

--- main.py
+++ main.py
@@ -14,9 +14,22 @@ from seed import seed_initial_data
 
 app = FastAPI(title="Orthopaedics Healthcare API")
 
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:5175",
+    "http://127.0.0.1:5175",
+    "https://orthofrontend.vercel.app"   # ← your production frontend
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(doctors.router)
